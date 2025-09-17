f35cda30c5c771b43a87efab727dd35ac8d3e225
Dzhamilia <jamilafatkullina@gmail.com>
2025-09-17 14:54:30 +0500

    added requested var code

--- main.py
+++ main.py
@@ -4,3 +4,17 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 import logging
 import jwt
 import time
+from dotenv import load_dotenv
+import os
+
+
+REQUIRED_VARS = {
+    "S3_ENDPOINT": os.getenv("S3_ENDPOINT"),
+    "S3_ACCESS_KEY": os.getenv("S3_ACCESS_KEY"),
+    "S3_SECRET_KEY": os.getenv("S3_SECRET_KEY"),
+    "S3_BUCKET": os.getenv("S3_BUCKET"),
+}
+
+for var_name, value in REQUIRED_VARS.items():
+    if not value or value.strip().lower() == "none":
+        raise ValueError(f"{var_name} не задан. Проверьте .env.")
